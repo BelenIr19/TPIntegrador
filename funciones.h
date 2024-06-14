02d2587e8740fdcf630464ef769dfe2a9e79e672
BelenIr19 <bi40725@gmail.com>
2024-06-14 10:11:47 -0300

    actualizacion

--- funciones.h
+++ funciones.h
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
+#include "rlutil.h"
+
 
 
 using namespace std;
@@ -10,13 +12,13 @@ using namespace std;
 ///Declaraciones
 
 void mostrarMenu();
-void cargarDadoManual (int dado[], int TAM);
-void cargarDadoAleatorio (int dado[], int TAM);
+void cargarDadoManual(int dado[], int TAM);
+void cargarDadoAleatorio(int dado[], int TAM);
 void mostrarValorDados(int dado[], int TAM);
 void puntajes(int dado[], int TAM);
-void escalera (int dado[], int TAM);
-void sexteto(int dado[], int TAM);
-void suma(int dado[], int TAM);
+bool escalera (int dado[], int TAM);
+bool sexteto(int dado[], int TAM);
+bool suma(int dado[], int TAM);
 
 
 
@@ -29,14 +31,14 @@ void mostrarMenu()
     cout<<" ---- Escalera o Cien ---- "<<endl;
     cout<<" 1 - Cargar Dado Manual "<<endl;
     cout<<" 2 - Cargar Dado Aleatorio "<<endl;
-    cout<<" 3 - Mostrar Vector "<<endl;
+    cout<<" 3 - Mostrar Dado "<<endl;
     cout<<" 4 - Mostrar Puntaje" << endl;
     cout<<" 0 - Salir "<<endl;
     cout<<" Ingrese la opcion deseada: "<<endl;
 }
 
 
-void cargarDadoAleatorio (int dado[], int TAM)
+void cargarDadoAleatorio(int dado[], int TAM)
 {
     int i;
     for (i=0; i<TAM ; i++ )
@@ -50,7 +52,7 @@ void cargarDadoAleatorio (int dado[], int TAM)
 
 
 
-void cargarDadoManual (int dado[], int TAM)
+void cargarDadoManual(int dado[], int TAM)
 {
     int i;
     for (i=0; i<TAM ; i++ )
@@ -67,93 +69,135 @@ void mostrarValorDados(int dado[], int TAM)
     int i;
     for (i=0; i<TAM ; i++ )
     {
-        cout<<"Elemento "<<i+1<<" del vector: "<<dado[i]<<endl;
+        cout<<"Elemento "<<i+1<<" del dado: "<<dado[i]<<endl;
     }
     system("pause");
 }
 
-void escalera (int dado[], int TAM)
+bool escalera (int dado[], int TAM)
 {
     int esc, numesc;
-    int puntaje=0, contdif=0;
+    int contdif=0;
     for (esc=0; esc<6; esc++)
     {
         if (esc ==0)
         {
             numesc=dado[esc];
-            contdif++;
+            contdif=1;
+
         }
-        else if (dado[esc]!=numesc)
+        else if (esc!=0)
+
         {
-            numesc=dado[esc];
-            contdif++;
+            contdif=contdif+1;
         }
 
         if (contdif==6)
         {
+            return true;
+        }
+        else
+        {
 
+
+            return false;
+
+        }
+
+        return false;
+    }
+}
+    bool sexteto(int dado[], int TAM)
+    {
+        int sext, numdd;
+        int contig=0;
+        for (sext=0; sext<6; sext++)
+        {
+            if (sext==0)
+            {
+                numdd=dado[sext];
+                contig++;
+            }
+            else if (dado[sext]==numdd)
+            {
+                numdd=dado[sext];
+                contig++;
+            }
+
+            if (contig==6)
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+
+        }
+
+
+        return false;
+    }
+
+    bool suma(int dado[], int TAM)
+    {
+
+        int sum, numsum;
+        int contig=0;
+        for (sum=0; sum<6; sum++)
+        {
+            if (sum ==0)
+            {
+                numsum=dado[sum];
+                contig++;
+            }
+            else if (dado[sum]!=numsum)
+            {
+                numsum=dado[sum];
+                contig++;
+            }
+        }
+        if (contig>1 && contig<6)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void puntajes(int dado[], int TAM)
+    {
+
+        bool esEscalera = escalera(dado, TAM);
+
+        cout << "Este es el valor de esEscalera" << esEscalera << endl;
+
+        if (esEscalera)
+        {
             cout << "Escalera!" <<endl;
+
+        }
+
+        bool esSuma = suma(dado, TAM);
+
+        if (esSuma)
+        {
+            cout << "Suma!" <<endl;
+
+        }
+
+        bool esSexteto = sexteto(dado, TAM);
+
+        if (esSexteto==1)
+        {
+
+            cout << "Sexteto!" <<endl;
         }
 
     }
-
-}
-
-void sexteto(int dado[], int TAM)
-{
-    int sext, numdd;
-    int puntaje=0, contig=0;
-    for (sext=0; sext<6; sext++)
-    {
-        if (sext ==0)
-        {
-            numdd=dado[sext];
-            contig++;
-        }
-        else if (dado[sext]==numdd)
-        {
-            numdd=dado[sext];
-            contig++;
-        }
-
-        if (contig==6)
-        {
-
-            cout << "Sexteto de "<< numdd <<endl;
-        }
-
-    }
-
-
-}
-
-void suma(int dado[], int TAM)
-{
-
-    int sum, numsum;
-    int puntaje=0, contdif=0;
-    for (sum=0; sum<6; sum++)
-    {
-        if (sum ==0)
-        {
-            numsum=dado[sum];
-            contdif++;
-        }
-        else if (dado[sum]!=numsum)
-        {
-            numsum=dado[sum];
-            contdif++;
-        }
-
-        if (contdif!=6 && contdif!=0)
-        {
-
-            cout << "SUMA" <<endl;
-        }
-
-    }
-}
-
 
 
 #endif // FUNCIONES_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -2,23 +2,31 @@
 #include "funciones.h"
 #include <stdlib.h>
 #include <time.h>
+#include "rlutil.h"
+#include "colors.h"
 
 using namespace std;
 
 
 int main()
 {
-
+    rlutil::setColor(rlutil::GREY);
+    rlutil::setBackgroundColor(rlutil::LIGHTMAGENTA);
     srand(time(NULL));
     const int TAM=6;
     int dados[TAM]= {};
     int opcion=1;
-    while (opcion!=0){
+    while (opcion!=0)
+    {
+       /// cout << RED     <<BG_ROSE<< "This text is RED!      " << RESET << endl;
+        ///system("pause");
+
 
         mostrarMenu();
 
         cin>>opcion;
-        switch (opcion){
+        switch (opcion)
+        {
         case 1:
             cargarDadoManual(dados, TAM);
             break;
@@ -29,13 +37,9 @@ int main()
             mostrarValorDados(dados, TAM);
             break;
         case 4:
-            escalera(dados, TAM);
-            break;
-        case 5:
-            sexteto(dados, TAM);
-            break;
-        case 6:
-            suma(dados, TAM);
+            puntajes(dados, TAM);
+            system("pause");
+
             break;
 
         case 0:
